d76b47f44cde1190ed9ce587396d268102f38ae2
hallowf <alexandre.cortegaca@gmail.com>
2020-04-09 21:46:26 +0100

    breakout-5

--- main.lua
+++ main.lua
@@ -26,7 +26,8 @@ function love.load()
   gFrames = {
     ['paddles'] = GenerateQuadsPaddles(gTextures['main']),
     ['balls'] = GenerateQuadsBalls(gTextures['main']),
-    ['bricks'] = GenerateQuadsBricks(gTextures['main'])
+    ['bricks'] = GenerateQuadsBricks(gTextures['main']),
+    ['hearts'] = GenerateQuads(gTextures['hearts'], 10, 9)
   }
 
   push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
@@ -54,7 +55,9 @@ function love.load()
 
   gStateMachine = StateMachine {
     ['start'] = function() return StartState() end,
-    ['play'] = function() return PlayState() end
+    ['play'] = function() return PlayState() end,
+    ['serve'] = function() return ServeState() end,
+    ['game-over'] = function() return GameOverState() end
   }
   gStateMachine:change('start')
 
@@ -81,6 +84,26 @@ function love.keyboard.wasPressed(key)
   else
     return false
   end
+end
+
+function renderHealth(health)
+  local healthX = VIRTUAL_WIDTH - 100
+
+  for i = 1, health do
+    love.graphics.draw(gTextures['hearts'], gFrames['hearts'][1], healthX,4)
+    healthX = healthX + 11
+  end
+
+  for i = 1, 3 - health do
+    love.graphics.draw(gTextures['hearts'], gFrames['hearts'][2], healthX, 4)
+    healthX = healthX + 11
+  end
+end
+
+function renderScore(score)
+  love.graphics.setFont(gFonts['small'])
+  love.graphics.print('Score:', VIRTUAL_WIDTH - 60, 5)
+  love.graphics.printf(tostring(score), VIRTUAL_WIDTH - 50, 5, 40, 'right')
 end
 
 function love.draw()
