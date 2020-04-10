8f0bafc4e4a918cd5c621538cbc8bac7dc7e93ab
hallowf <alexandre.cortegaca@gmail.com>
2020-04-10 20:04:03 +0100

    breakout-10

--- main.lua
+++ main.lua
@@ -60,9 +60,12 @@ function love.load()
     ['play'] = function() return PlayState() end,
     ['serve'] = function() return ServeState() end,
     ['victory'] = function() return VictoryState() end,
+    ['high-scores'] = function() return HighScoreState() end,
     ['game-over'] = function() return GameOverState() end
   }
-  gStateMachine:change('start')
+  gStateMachine:change('start', {
+    highScores = loadHighScores()
+  })
 
   love.keyboard.keysPressed = {}
 end
@@ -132,6 +135,48 @@ function lowerSoundsVolume()
   for _, sound in pairs(gSounds) do
     sound:setVolume(0.2)
   end
+end
+
+function loadHighScores()
+  love.filesystem.setIdentity('CS50Breakout')
+
+  local fileInfo = love.filesystem.getInfo('breakout.lst')
+
+  if fileInfo.FileType == nil then
+    local scores = ''
+    for i = 10, 1, -1 do
+      scores = scores .. 'CTO\n'
+      scores = scores .. tostring(i * 1000) .. '\n'
+    end
+
+    love.filesystem.write('breakout.lst', scores)
+  else
+    local name = true
+    local currentName = nil
+    local counter = 1
+
+    local scores = {}
+
+    for i = 1, 10 do
+      scores[i] = {
+        name = nil,
+        score = nil
+      }
+    end
+
+    for line in love.filesystem.lines('breakout.lst') do
+      if name then
+        scores[counter].name = string.sub(line, 1, 3)
+      else
+        scores[counter].score = tonumber(line)
+        counter = counter + 1
+      end
+
+      name = not name
+    end
+  end
+
+  return scores
 end
 
 function displayFPS()
