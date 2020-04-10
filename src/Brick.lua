06ae4052365fc9d433836c28151476216844e8b1
hallowf <alexandre.cortegaca@gmail.com>
2020-04-10 18:25:14 +0100

    breakout-8

--- src/Brick.lua
+++ src/Brick.lua
@@ -1,5 +1,38 @@
 Brick = Class{}
 
+paletteColors = {
+  -- blue
+  [1] = {
+    ['r'] = 99/255,
+    ['g'] = 155/255,
+    ['b'] = 1
+  },
+  -- green
+  [2] = {
+    ['r'] = 106/255,
+    ['g'] = 190/255,
+    ['b'] = 47/255
+  },
+  -- red
+  [3] = {
+    ['r'] = 217/255,
+    ['g'] = 87/255,
+    ['b'] = 99/255
+  },
+  -- purple
+  [4] = {
+    ['r'] = 215/255,
+    ['g'] = 123/255,
+    ['b'] = 186/255
+  },
+  -- gold
+  [5] = {
+    ['r'] = 251/255,
+    ['g'] = 242/255,
+    ['b'] = 54/255
+  }
+}
+
 function Brick:init(x, y)
   self.tier = 0
   self.color = 1
@@ -10,9 +43,30 @@ function Brick:init(x, y)
   self.height = 16
 
   self.inPlay = true
+
+  self.psystem = love.graphics.newParticleSystem(gTextures['particle'], 64)
+
+  self.psystem:setParticleLifetime(0.5, 1)
+
+  self.psystem:setLinearAcceleration(-15, 0, 15, 80)
+
+  self.psystem:setEmissionArea('normal', 10, 10)
 end
 
 function Brick:hit()
+
+  self.psystem:setColors(
+    paletteColors[self.color].r,
+    paletteColors[self.color].g,
+    paletteColors[self.color].b,
+    55 * (self.tier + 1),
+    paletteColors[self.color].r,
+    paletteColors[self.color].g,
+    paletteColors[self.color].b,
+    0
+  )
+  self.psystem:emit(64)
+
   gSounds['brick-hit-2']:stop()
   gSounds['brick-hit-2']:play()
 
@@ -37,6 +91,10 @@ function Brick:hit()
   end
 end
 
+function Brick:update(dt)
+  self.psystem:update(dt)
+end
+
 function Brick:render()
   if self.inPlay then
     love.graphics.draw(gTextures['main'],
@@ -44,4 +102,8 @@ function Brick:render()
       self.x, self.y
     )
   end
+end
+
+function Brick:renderParticles()
+  love.graphics.draw(self.psystem, self.x + 16, self.y + 8)
 end
--- src/states/PlayState.lua
+++ src/states/PlayState.lua
@@ -90,6 +90,10 @@ function PlayState:update(dt)
     end
   end
 
+  for k, brick in pairs(self.bricks) do
+    brick:update(dt)
+  end
+
   if love.keyboard.wasPressed('escape') then
     love.event.quit()
   end
@@ -98,6 +102,10 @@ end
 function PlayState:render()
   for k, brick in pairs(self.bricks) do
     brick:render()
+  end
+
+  for k, brick in pairs(self.bricks) do
+    brick:renderParticles()
   end
 
   self.paddle:render()
